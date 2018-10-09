975385895dd0bd9193df8d20c64a4c0d09456eb1
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-10-09 15:25:06 +0200

    Added move API

--- src/main/java/com/codecool/mhmm/punkrock_stickman/controller/StartGame.java
+++ src/main/java/com/codecool/mhmm/punkrock_stickman/controller/StartGame.java
@@ -1,10 +1,14 @@
 package com.codecool.mhmm.punkrock_stickman.controller;
 
 import com.codecool.mhmm.punkrock_stickman.Game;
+import com.codecool.mhmm.punkrock_stickman.model.game_objects.characters.Player;
+import com.codecool.mhmm.punkrock_stickman.model.map.Level;
 import com.codecool.mhmm.punkrock_stickman.service.JSONHandler;
+import com.codecool.mhmm.punkrock_stickman.service.MoveHandler;
 import com.codecool.mhmm.punkrock_stickman.service.Sound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +19,9 @@ public class StartGame {
 
     @Autowired
     private JSONHandler jsonHandler;
+
+    @Autowired
+    private MoveHandler moveHandler;
 
     @GetMapping("/send")
     public String initGame() {
@@ -31,5 +38,27 @@ public class StartGame {
         Sound.init();
 
         return jsonHandler.gameStateToJson(game.getPlayer(), game.getLevel(), "Game loaded");
+    }
+
+    @GetMapping("/move")
+    public String move(@RequestHeader("move") String move) {
+
+        Player player = game.getPlayer();
+        Level level = game.getLevel();
+
+        if (move.equals("down") && player.getY() < level.getHEIGHT() - 1) {
+            moveHandler.moveDown(player);
+        }
+        if (move.equals("up") && player.getY() > 0) {
+            moveHandler.moveUp(player);
+        }
+        if (move.equals("right") && player.getX() < level.getWIDTH() - 1) {
+            moveHandler.moveRight(player);
+        }
+        if (move.equals("left") && player.getX() > 0) {
+            moveHandler.moveLeft(player);
+        }
+
+        return jsonHandler.gameStateToJson(game.getPlayer(), game.getLevel(), "Move to the " + move);
     }
 }
